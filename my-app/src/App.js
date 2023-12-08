7133ceca64650db5292c01817af1fbff22239a12
Ekterina Goryainova <kateas27@mail.ru>
2023-12-08 14:57:39 +0200

    edit code without jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,40 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createElement } from "react";
 
 const dateNow = new Date(); //декларативный
 const yearNow = dateNow.getFullYear();
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{yearNow}</p>
-			</header>
-		</div>
+	return createElement(
+		"div",
+		{ className: "App" },
+		createElement(
+			"header",
+			{ className: "App-header" },
+			createElement("img", {
+				src: `${logo}`,
+				className: "App-logo",
+				alt: "logo",
+			}),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", { children: `src/App.js` }),
+				" and save to reload."
+			),
+			createElement(
+				"a",
+				{
+					className: "App-link",
+					href: "https://reactjs.org",
+					target: "_blank",
+					rel: "noopener noreferrer",
+				},
+				"Learn React"
+			),
+			createElement("p", { children: `${yearNow}` })
+		)
 	);
 }
 
